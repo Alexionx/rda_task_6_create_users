0a47191112f9a97179df842504873c81183527eb
KempachiDZ <alexmelnyk48370@gmail.com>
2025-02-21 20:31:06 +0200

    Solve task

--- task.sql
+++ task.sql
@@ -1,1 +1,12 @@
 -- write your code for database user creation here
+​​
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+​​GRANT INSERT, UPDATE, DELETE, SELECT ON database.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL ON database.* TO 'deploymentuser'@'%';
+
+
+
